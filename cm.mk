335ec49e09f377470453b0bae931e24f091c5e5c
AragaoAnderson <andersonaragao@me.com>
2015-05-18 23:53:47 -0300

    Quark: Fix somethings
    Change-Id: I9e593d6214e47a3713e0e8333a7b0ac5c134eacc

--- cm.mk
+++ cm.mk
@@ -11,4 +11,6 @@ PRODUCT_NAME := cm_quark
 NX_MODEL_NAME := Quark
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=quark
+    PRODUCT_NAME=quark_retbr \
+    BUILD_FINGERPRINT=motorola/quark_retbr/quark_umts:5.0.2/LXG22.33-12.13/13:user/release-keys \
+    PRIVATE_BUILD_DESC="quark_retbr-user 5.0.2 LXG22.33-12.13 13 release-keys"
