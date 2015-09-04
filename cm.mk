82f097d57add88ecb07355f2fe8166f586f9143b
Dan Pasanen <dan.pasanen@gmail.com>
2015-09-04 07:50:13 -0700

    overlay: select ipv6 apn by default on t-mobile
    * also ensure overlay overrides anything in vendor/cm
    
    Change-Id: I288df30cc3bc1800617e31794b2c4729cea525e7

--- cm.mk
+++ cm.mk
@@ -6,6 +6,9 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
 PRODUCT_RELEASE_NAME := Moto MAXX
 PRODUCT_NAME := cm_quark
 NX_MODEL_NAME := Quark
